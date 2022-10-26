5bf1dbd8176c6cf29f63a0819043a0a87738f1e5
tyson lind <tysonfloydlind@gmail.com>
2022-10-26 11:23:26 -0600

    changed dataPlan function name

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ triangleType(1,2,2);
 console.log("EXERCISE 5:\n==========\n");
 // Exercise 5 Section
 
-function dataPlanStatus (planLimit, day, usage) {
+function dataPlan (planLimit, day, usage) {
 const remainingDays = 30 - day;
 const avgUse = Math.round((usage/day) * 100)/100;
 const netAvg = Math.round((planLimit/30) * 100) / 100;
@@ -131,6 +131,6 @@ else if (avgUse < netAvg) {
 
 }
 
-dataPlanStatus(100,12,55);
-dataPlanStatus(500,12,15);
-dataPlanStatus(30,1,1);
+dataPlan(100,12,55);
+dataPlan(500,12,15);
+dataPlan(30,1,1);
